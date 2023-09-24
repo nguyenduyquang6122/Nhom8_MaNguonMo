caeaae6e8fb834f69ed1fa6436257208af9900e3
unknown <nguyenduyquang6122@gmail.com>
2023-09-24 22:40:23 +0700

    Add enter the number of rows, columns of the matrix and use numpy.transpose()

---
+++ improveNhom2.py
@@ -0,0 +1,25 @@
+import numpy as np #import numpy
+
+matrix = []
+
+print("Enter the number of rows, columns of the matrix") #create matrix r*c
+r = int(input("Enter rows: ")) 
+c = int(input("Enter columns: "))
+
+for x in range(r*c):
+    matrix.append(int(input("Enter value "+ str(x) +" : "))) #created empty list and enter value
+
+arr = np.array(matrix, ndmin = 1) #convert list into array
+newarr = arr.reshape(r,c) #create a rxc 2D array
+x = newarr.copy() #create copy of rxc array
+
+#DISPLAY ALL RELEVANT INFO
+print("Following is the matrix you entered:")
+print("---------------------------------------")
+print(newarr)
+print("---------------------------------------")
+
+print("Following is the transposed matrix:")
+print("---------------------------------------")
+print(x.transpose())
+print("---------------------------------------")
